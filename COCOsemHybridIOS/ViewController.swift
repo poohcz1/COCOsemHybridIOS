62265133fd00d962bc692da09b5ab19b8380f954
poohcz <poohcz@nate.com>
2022-02-28 08:59:28 +0900

    Initial Commit

---
+++ COCOsemHybridIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COCOsemHybridIOS
+//
+//  Created by kpMBP on 2022/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
